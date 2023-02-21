c55f5bd372fb25b02e3d811155e4dcaf81b2f3e7
Lukasz Poznanski <l.poznanski3@gmail.com>
2023-02-21 12:17:45 +0100

    2.2. task - added post model, post related views + templates

--- 2. Backend - Python/2.2./blog/blog_app/views.py
+++ 2. Backend - Python/2.2./blog/blog_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Post
+class PostListView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
 
-# Create your views here.
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class LoginView(View):
+    pass
+
