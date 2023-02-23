cb4b5db3b7b9da96188bbca88ad238e3c6d49c27
Lukasz Poznanski <l.poznanski3@gmail.com>
2023-02-23 11:47:36 +0100

    2.2. task - added CSS style

--- 2. Backend - Python/2.2./blog/blog_app/forms.py
+++ 2. Backend - Python/2.2./blog/blog_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PostForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
